6b25eaff158dc86dc04729f6f155bf47df5f01f9
Madhan-404 <madhansainik@gmail.com>
2023-06-16 20:38:55 +0530

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+/**
+ * Constants used throughout the samples
+ */
+
+import { Cluster } from '@solana/web3.js';
+
+export const DEVNET_RPC_URL = 'https://api.devnet.solana.com';
+
+export const CLUSTER: Cluster = 'devnet';
+
+/**
+ * ONLY FOR SAMPLES NEVER EVER STORE YOUR/ANYONE'S PRIVATE KEY IN PLAIN TEXT
+ * TODO: Insert your private key here
+ */
+export const PRIV_KEY = new Uint8Array([133, 86, 83, 3, 111, 178, 20, 215, 165, 205, 20, 40, 94, 55, 255, 48, 150, 46, 75, 50, 163, 188, 179, 46, 152, 66, 178, 251, 178, 150, 175, 201, 34, 215, 126, 217, 224, 40, 119, 253, 163, 107, 13, 2, 206, 234, 167, 152, 179, 226, 133, 93, 29, 86, 112, 71, 186, 132, 255, 244, 7, 29, 147, 110]);
